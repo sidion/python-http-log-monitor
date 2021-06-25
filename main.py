d036ebb43da7c05552c80851122ff0f5c43c23bd
KReed <kevin.reed@ualberta.net>
2021-06-25 13:41:59 -0700

    adding curses for display

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import collections
 import sys
 import argparse
+
+import curses
 
 import time
 import subprocess
@@ -8,7 +11,53 @@ import select
 import apache_log_parser
 from apache_log_parser import LineDoesntMatchException
 
-if __name__ == '__main__':
+from collections import deque
+
+def c_main(stdscr: 'curses._CursesWindow', log_file) -> int:
+    #the use of sh.tail will limit this appliation to only working on *nix systems with the tail command
+    f = subprocess.Popen(['tail','-F', log_file], stdout=subprocess.PIPE )
+    p = select.poll()
+    p.register(f.stdout)
+
+    log_parser = apache_log_parser.make_parser('%h %u %l %t "%r" %s %B')
+
+    size_of_top_data = 7
+
+    #-1 required to keep the bottom line of the terminal for the curses and prevent curses exception
+    number_of_logs_to_show = curses.LINES - size_of_top_data -1
+    
+    display_log_queue = collections.deque([],number_of_logs_to_show)
+
+    stdscr.nodelay(True) #prevent blocking input
+
+    #main process loop
+    while True:
+        #render data
+        stdscr.clear()
+        stdscr.addstr(0,0, "HTTP Log monitor - press any key to exit")
+        stdscr.addstr(1,0, "STATUS: Nominal")
+        for idx, log in enumerate(display_log_queue):
+            stdscr.addstr(idx + size_of_top_data, 0, log[:curses.COLS])
+
+        stdscr.refresh()
+
+        # injest new data
+        if p.poll(1): 
+            try:
+                log_line = f.stdout.readline()
+                display_log_queue.append(log_line)
+                parts = log_parser(str(log_line))
+            except LineDoesntMatchException:
+                print(f"log found that did not match parsing: {log_line}", file=sys.stderr)
+                pass
+
+        time.sleep(1)    
+        if stdscr.getch() != curses.ERR:
+            #user input detected exit
+            break
+    
+ 
+def main() -> int:
     #Default values
     #TODO: move to config file
     FILE_LOCATION = '/tmp/access.log'
@@ -22,23 +71,10 @@ if __name__ == '__main__':
         if arguments.file:
             FILE_LOCATION = arguments.file
 
-    #the use of sh.tail will limit this appliation to only working on *nix systems with the tail command
-    f = subprocess.Popen(['tail','-F', FILE_LOCATION], stdout=subprocess.PIPE )
-    p = select.poll()
-    p.register(f.stdout)
 
-    log_parser = apache_log_parser.make_parser('%h %u %l %t "%r" %s %B')
+    return curses.wrapper(c_main, FILE_LOCATION)
 
 
-    while True:
-        if p.poll(1):
-            try:
-                log_line = f.stdout.readline()
-                parts = log_parser(str(log_line))
-                print(parts)
-            except LineDoesntMatchException:
-                print(f"log found that did not match parsing: {log_line}", file=sys.stderr)
-                pass
-        time.sleep(1)    
-    
-        
+if __name__ == '__main__':
+    exit(main())
+      
