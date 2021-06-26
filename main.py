1c7e9993f056931a7f192e7d18b8f0d7b981df86
KReed <kevin.reed@ualberta.net>
2021-06-26 15:17:55 -0700

    restructure to seperate application vs loading responsibilities

--- main.py
+++ main.py
@@ -2,108 +2,11 @@ from logreporter import LogReporter
 import sys
 import argparse
 import configparser
-
 import curses
 
-import time
-import subprocess
-import select
-
-import apache_log_parser
-from apache_log_parser import LineDoesntMatchException
-
-import collections
-from collections import deque
-
-import datetime
-
-
-def c_main(stdscr: 'curses._CursesWindow', log_file, alert_window, alert_threshold, stat_window) -> int:
-    #the use of sh.tail will limit this appliation to only working on *nix systems with the tail command
-    f = subprocess.Popen(['tail','-F', log_file], stdout=subprocess.PIPE )
-    p = select.poll()
-    p.register(f.stdout)
-
-    log_parser = apache_log_parser.make_parser('%h %u %l %t "%r" %s %B')
-
-    size_of_top_data = 8
-
-    #-1 required to keep the bottom line of the terminal for the curses and prevent curses exception
-    number_of_logs_to_show = curses.LINES - size_of_top_data -1
-    
-    display_log_queue = collections.deque([],number_of_logs_to_show)
-    
-    #set log retention length to alter window + stat window to ensure we always have the data we need
-    #but don't introduce any memory leaks
-    reporter = LogReporter(alert_window + stat_window)
-    stats = {}
-
-    stdscr.nodelay(True) #prevent blocking input
-
-    
-    current_state = 'Nominal'
-    alert_string = ''
-    current_window = ''
-    #main process loop
-    while True:
-        now = datetime.datetime.now()
-        if now.second % stat_window == 0:
-            current_window = now.strftime("%H:%M:%S") + ' - ' + ( now - datetime.timedelta(seconds=stat_window) ).strftime('%H:%M:%S')
-            stats = reporter.getStatsForWindow(stat_window)
-
-            requests_per_sec_in_alert_window = reporter.getRequestPerSecondForWindow(alert_window)
-            if requests_per_sec_in_alert_window >= alert_threshold:
-                current_state = 'ALERT'
-                alert_string = f" High traffic generated an alert - hits = {requests_per_sec_in_alert_window}, triggered at {now.strftime('%H:%M:%S')}"                
-            elif current_state == 'ALERT':
-                current_state = "RECOVERED"
-                alert_string = ''
-            else:
-                current_state = 'Nominal'
-
-            reporter.pruneLogs()
-
-        #render data
-        stdscr.clear()
-        title = "HTTP Log monitor - press any key to exit"
-        title_location = int((curses.COLS / 2) - ( len(title) /2 ))
-        stdscr.addstr(0,title_location, title)
-        stdscr.addstr(1,0, f"Stats for window: {current_window}")
-        stdscr.addstr(2,0, f"STATUS: {current_state}")
-        if current_state == 'ALERT':
-            stdscr.addstr(2, 14, alert_string)
-        line_index = 3
-        
-        for key in stats:
-            name = key.replace("_", " ")
-            display_string = f"{name}: {stats[key]}"
-            stdscr.addstr(line_index, 0, display_string[:curses.COLS])
-            line_index += 1
-        
-        for idx, log in enumerate(display_log_queue):
-            stdscr.addstr(idx + size_of_top_data, 0, log[:curses.COLS])
-
-        stdscr.refresh()
-
-        # injest new data
-        while p.poll(1): 
-            try:
-                log_line = f.stdout.readline()
-                display_log_queue.append(log_line)
-                reporter.addLog( log_parser(str(log_line)) )
-            except LineDoesntMatchException:
-                print(f"log found that did not match parsing: {log_line}", file=sys.stderr)
-                pass
-
-        time.sleep(1)    
-        if stdscr.getch() != curses.ERR:
-            #user input detected exit
-            break
-    
+import httplogparser
  
 def main() -> int:
-    #Default values
-
     config = configparser.ConfigParser()
     config.read('config.ini')
     default_config = config['default']
@@ -131,7 +34,7 @@ def main() -> int:
         if arguments.statwindow:
             STAT_WINDOW_LENGTH =int(arguments.statwindow)
 
-    return curses.wrapper(c_main, FILE_LOCATION, ALERT_WINDOW_LENGTH, ALERT_THRESHOLD, STAT_WINDOW_LENGTH)
+    return curses.wrapper(httplogparser.HttpLogParser.c_main, FILE_LOCATION, ALERT_WINDOW_LENGTH, ALERT_THRESHOLD, STAT_WINDOW_LENGTH)
 
 
 if __name__ == '__main__':
